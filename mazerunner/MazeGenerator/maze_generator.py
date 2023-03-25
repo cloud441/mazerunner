72cde2d4a4463d4b4b734b77a08fae8658e21030
cloud441 <57587844+cloud441@users.noreply.github.com>
2023-03-25 19:07:43 +0100

    chore: change init maze generator size

--- mazerunner/MazeGenerator/maze_generator.py
+++ mazerunner/MazeGenerator/maze_generator.py
@@ -2,8 +2,8 @@ from mazerunner.MazeGenerator.maze import Maze
 
 
 class MazeGenerator:
-    def __init__(self):
-        self.maze = Maze(10)
+    def __init__(self, size: int = 10):
+        self.maze = Maze(size=size)
 
     def generate_maze(self, size: int) -> Maze:
         return self.maze
