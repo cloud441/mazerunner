ffcdf853ae9fda5f162aebcf98d007b74447df4c
cloud441 <57587844+cloud441@users.noreply.github.com>
2023-03-25 20:20:40 +0100

    feat: begin implementation of recursive BT for maze generation

--- mazerunner/MazeGenerator/maze.py
+++ mazerunner/MazeGenerator/maze.py
@@ -1,12 +1,24 @@
-class Maze:
-    def __init__(self, size: int):
-        self.board = [[0 for _ in range(size)] for _ in range(size)]
+import numpy as np
 
-    def get_board(self):
+
+class Maze:
+    def __init__(self, height: int, width: int):
+        self.board = np.ones((height, width), dtype=np.int8)
+        self.board[1:height - 1, 1:width - 1] = 0
+
+    def __getitem__(self, x: int, y: int) -> int:
+        return self.board[x, y]
+
+    def __setitem__(self, x: int, y: int, value: int):
+        self.board[x, y] = value
+
+    def get_board(self) -> np.array:
         return self.board
 
-    def print_board(self):
+    def __str__(self):
+        str_board = ""
         for row in self.board:
             for cell in row:
-                print(cell, end=" ")
-            print()
+                str_board += str(cell) + " "
+            str_board += "\n"
+        return str_board
--- mazerunner/MazeGenerator/maze_generator.py
+++ mazerunner/MazeGenerator/maze_generator.py
@@ -1,9 +1,32 @@
 from mazerunner.MazeGenerator.maze import Maze
+from enum import Enum
+
+
+class GenerationAlgoEnum(str, Enum):
+    RECURSIVE_BT = "recursive_backtracking"
 
 
 class MazeGenerator:
-    def __init__(self, size: int = 10):
-        self.maze = Maze(size=size)
+    def __init__(self):
+        self.height = 2
+        self.width = 2
+        self.maze: Maze = Maze(2, 2)
 
-    def generate_maze(self, size: int) -> Maze:
+    def generate_maze(self, algorithm: GenerationAlgoEnum, height: int = 10, width: int = 10) -> Maze:
+        self.height = height
+        self.width = width
+        self.maze = Maze(height=height, width=width)
+
+        if algorithm == GenerationAlgoEnum.RECURSIVE_BT:
+            self.generate_recursive_backtracking()
+
         return self.maze
+
+    def generate_recursive_backtracking(self):
+        # build a checkerboard before backtracking:
+        for i in range(self.height):
+            for j in range(self.width):
+                if i % 2 == 1 or j % 2 == 1:
+                    self.maze[i, j] = 0
+                else:
+                    self.maze[i, j] = 1
