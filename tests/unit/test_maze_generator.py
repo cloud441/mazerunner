e6a105b71ff92c709538c5d308599b4e2cc9a93f
cloud441 <57587844+cloud441@users.noreply.github.com>
2023-03-25 19:22:48 +0100

    style: fix coding style for test

--- tests/unit/test_maze_generator.py
+++ tests/unit/test_maze_generator.py
@@ -3,4 +3,6 @@ from mazerunner.MazeGenerator.maze_generator import MazeGenerator
 
 def test_maze_generator_init():
     maze_generator = MazeGenerator()
-    assert maze_generator.maze.get_board() == [[0 for _ in range(10)] for _ in range(10)]
+    assert maze_generator.maze.get_board() == [
+        [0 for _ in range(10)] for _ in range(10)
+    ]
